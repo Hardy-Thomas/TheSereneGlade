869005bc747d92dd1d2c54ffca6ee32bb738e107
Dragonhail <thomardy92@gmail.com>
2025-10-02 23:50:08 +0200

    add modification

--- porte_cript.gd
+++ porte_cript.gd
@@ -1,14 +1,13 @@
 extends Area2D
 
 
-# Nom de la scène à charger
 @export var scene_to_load: PackedScene
 
-# Signal quand quelque chose entre dans la zone
-func _on_area_entered(area):
-	if area.is_in_group("player"):
-		get_tree().change_scene_to(scene_to_load)
 
-# Connexion du signal
-func _ready():
-	connect("area_entered", Callable(self, "_on_area_entered"))
+#func _ready():
+	#connect("area_entered", Callable(self, "_on_area_entered"))
+
+
+func _on_area_entered(area: Area2D) -> void:
+	if area.is_in_group("Players"):
+		get_tree().change_scene_to(scene_to_load)
