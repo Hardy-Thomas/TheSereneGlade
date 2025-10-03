fcf68e70a9422fa672c1008df4bddf33dfc305f0
Dragonhail <thomardy92@gmail.com>
2025-10-03 16:00:28 +0200

    resolve probleme

--- porte_cript.gd
+++ porte_cript.gd
@@ -9,12 +9,12 @@ extends Area2D
 
 
 func _on_area_entered(area: Area2D) -> void:
-	if area.is_in_group("Players"):
+	if area.is_in_group("Player"):
 		#var fade_timer = Timer.new()
 		#fade_timer.wait_time = 1
 		#fade_timer.one_shot = true
 		#add_child(fade_timer)
 		#fade_timer.start()
 		#fade_timer.timeout.connect(func(): 
-		var fade_manager = get_tree().get_first_node_in_group("fade")
+		var fade_manager = get_tree().get_first_node_in_group("FadeInOut")
 		fade_manager.fade_to_scene("res://cript.tscn")
