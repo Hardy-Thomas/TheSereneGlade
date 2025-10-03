c668e21f489782f66bdad9db86baf7b422530ef1
Dragonhail <thomardy92@gmail.com>
2025-10-03 16:15:06 +0200

    resolve probleme

--- porte_cript.gd
+++ porte_cript.gd
@@ -17,4 +17,4 @@ func _on_area_entered(area: Area2D) -> void:
 		#fade_timer.start()
 		#fade_timer.timeout.connect(func(): 
 		var fade_manager = get_tree().get_first_node_in_group("FadeInOut")
-		fade_manager.fade_to_scene("res://cript.tscn")
+		fade_manager.fade_to_scene("res://crypt.tscn")
