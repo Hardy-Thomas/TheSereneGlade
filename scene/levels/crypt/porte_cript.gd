070d654b6f68cf31eac30d7412186f478791623e
Dragonhail <thomardy92@gmail.com>
2025-10-04 20:36:05 +0200

    minor change

--- scene/levels/crypt/porte_cript.gd
+++ scene/levels/crypt/porte_cript.gd
@@ -10,11 +10,5 @@ extends Area2D
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group("Player"):
-		#var fade_timer = Timer.new()
-		#fade_timer.wait_time = 1
-		#fade_timer.one_shot = true
-		#add_child(fade_timer)
-		#fade_timer.start()
-		#fade_timer.timeout.connect(func(): 
 		var fade_manager = get_tree().get_first_node_in_group("FadeInOut")
 		fade_manager.fade_to_scene("res://scene/levels/crypt/crypt.tscn")
